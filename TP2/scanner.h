bb91292e96ee88ab049864774dab1d0d983c4970
Matias Bisceglia <matiasbisceglia03@gmailcom>
2026-09-12 14:52:48 -0300

    corrigiendo struct token

--- TP2/scanner.h
+++ TP2/scanner.h
@@ -4,22 +4,21 @@
 #include <ctype.h>
 
 typedef enum{
-    TOKEN_CONST= 100,
-    TOKEN_SUMA= 101,
-    TOKEN_RESTA= 102,
-    TOKEN_MULT= 103,
-    TOKEN_DIV= 104,
-    TOKEN_EXP= 105,
-    TOKEN_ASIG= 106,
-    TOKEN_ASIG_SUME= 107,
-    TOKEN_ASIG_RESTA= 108,
-    TOKEN_ASIG_MULT= 109,
-    TOKEN_ASIG_DIV= 110,
-    TOKEN_FDT= 111, //Fin del lexema
-    TOKEN_VAR= 112,
+    TOKEN_SUMA= 101,        // +
+    TOKEN_RESTA= 102,       // -
+    TOKEN_MULT= 103,        // *
+    TOKEN_DIV= 104,         // /
+    TOKEN_EXP= 105,         // ^
+    TOKEN_ASIG= 106,        // =
+    TOKEN_ASIG_SUMA= 107,   // +=
+    TOKEN_ASIG_RESTA= 108,  // -=
+    TOKEN_ASIG_MULT= 109,   // *=
+    TOKEN_ASIG_DIV= 110,    // /=
+    TOKEN_FDT= 111,         // Fin del lexema
+    TOKEN_VAR= 112,         // Variable
+    TOKEN_CONST= 100,       // Constante
 
-    ERROR_GRAL= 401,
-    ERROR_LEXICO= 402
+    ERROR= 401
 } Token;
 
 void scanner(char* entrada);
